76c417c84d2c6f90d7f8a6821be13018927cf147
Daniel Grießhaber <dangrie158@gmail.com>
2016-04-29 23:35:41 +0200

    added guards to avoid redefinition to headers

--- Firmware/src/bitmaps.h
+++ Firmware/src/bitmaps.h
@@ -1,3 +1,6 @@
 #include "Bitmap.h"
 
+#ifndef _ALL_BITMAPS_H_
+#define _ALL_BITMAPS_H_
 extern const Bitmap nupoLogo;
+#endif //_ALL_BITMAPS_H_
--- Firmware/src/fonts.h
+++ Firmware/src/fonts.h
@@ -1,4 +1,7 @@
 #include "Font.h"
 
+#ifndef _ALL_FONTS_H_
+#define _ALL_FONTS_H_
 extern const Font smallFont;
 extern const Font bigFont;
+#endif //_ALL_FONTS_H_
