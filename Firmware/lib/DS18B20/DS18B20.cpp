4e86cec168e6384a23e164a22690bf9ecfa9845d
Daniel Grießhaber <dangrie158@gmail.com>
2016-06-15 08:59:19 +0200

    added functions to asynchronously request the temperature from DS18B20 sensors

--- Firmware/lib/DS18B20/DS18B20.cpp
+++ Firmware/lib/DS18B20/DS18B20.cpp
@@ -1,18 +1,28 @@
 #include "DS18B20.h"
 
-DS18B20::DS18B20(uint8_t pinNumber) : mPin(pinNumber) {
+DS18B20::DS18B20(uint8_t pinNumber)
+    : mPin(pinNumber), mAddress(new uint8_t[8]) {
   mIO = new OneWire(mPin);
   mSensor = new DallasTemperature(mIO);
 
   mSensor->begin();
+  mSensor->setWaitForConversion(false);
+  mSensor->getAddress(mAddress, FIRST_DEVICE);
 }
 
 DS18B20::~DS18B20() {
   delete mSensor;
   delete mIO;
+  delete mAddress;
 }
 
 float DS18B20::getTemperature() {
-  mSensor->requestTemperatures();
   return mSensor->getTempCByIndex(FIRST_DEVICE);
+}
+
+void DS18B20::requestTemperature() {
+  mSensor->requestTemperaturesByAddress(mAddress);
+}
+bool DS18B20::isConversionReady() {
+  return mSensor->isConversionAvailable(mAddress);
 }
--- Firmware/lib/DS18B20/DS18B20.h
+++ Firmware/lib/DS18B20/DS18B20.h
@@ -13,12 +13,15 @@ private:
   uint8_t mPin;
   OneWire *mIO;
   DallasTemperature *mSensor;
+  uint8_t *mAddress;
 
 public:
   DS18B20(uint8_t pinNumber);
   ~DS18B20();
 
   float getTemperature();
+  void requestTemperature();
+  bool isConversionReady();
 };
 
 #endif // DS18B20_H_
